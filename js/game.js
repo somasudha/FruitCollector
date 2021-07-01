2a3322ab6bd5be67298413bbf96860fdc437f9c1
somasudha <somasudha@gmail.com>
2021-07-01 15:49:44 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -58,7 +58,9 @@ class Game{
                      if(index === player.index){
                          
                        //add code to display the player's name on the respective basket.
-                       text(allPlayers[plr].name, x-25, y+25);
+                       fill("black")
+                       textSize(25)
+                       text(allPlayers[plr].name,x-25,y+25);
                          
                      }
                     
